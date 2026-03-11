e3c196ee982c3e7b5206ee75f1fee8bd9530007b
GitLakshman <lakshmanprasad13579@gmail.com>
2026-03-11 11:51:46 +0530

    removed github workflow due to work configuration

---
+++ client/src/constsants/index.ts
@@ -0,0 +1,12 @@
+export const AppConstants = {
+  RAZORPAY_KEY_ID: "rzp_test_SGivitgaITImgH",
+};
+
+export const DiscountConstants = {
+  NONE: 0,
+  SVVM08: 8,
+  SVVM10: 10,
+  SVVM12: 12,
+  SVVM16: 16,
+  SVVM18: 18,
+};
